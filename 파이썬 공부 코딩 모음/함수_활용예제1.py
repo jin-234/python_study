5c9bdda7095f86d62330dbd15dd173e2a4792f6f
jin-234 <kmjin041010@gmail.com>
2026-05-21 15:24:46 +0900

    전달된 문자열을 암호화/복호화 시켜서 반환하는 함수

--- 파이썬 공부 코딩 모음/함수_활용예제1.py
+++ 파이썬 공부 코딩 모음/함수_활용예제1.py
@@ -15,6 +15,6 @@ stringdata='I love ai python programming'
 EncBook={'1':'#','p':'@','o':'7','g':'$','I':'%','a':'8','t':'*','r':'3','n':'6'}
 encoding=EncryptFunc(stringdata)#전달된 문자열을 암호화 시켜서 반환하는 함수
 print(encoding)
-#위 암호화된 문자열 다시 복원시키는 함수 구현
+#위 암호화된 문자열 다시 복원시키는 함수 구현 
 decmsg=DecryptFunc(encoding)
 print(decmsg)
