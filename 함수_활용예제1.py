b81e38c3fe4c0d476025e34637fc15c9984ed3dc
jin-234 <kmjin041010@gmail.com>
2026-05-21 14:15:52 +0900

    Add files via upload
    암호화 복호화 함수

---
+++ 함수_활용예제1.py
@@ -0,0 +1,20 @@
+def EncryptFunc(msg):
+    for alph in msg:
+        if alph in EncBook:
+            msg=msg.replace(alph,EncBook[alph])
+    return msg
+def DecryptFunc(msg):
+    DncBook={}
+    for key in EncBook:
+        DncBook[EncBook[key]]=key
+    for alph in msg:
+        if alph in DncBook:
+            msg=msg.replace(alph,DncBook[alph])
+    return msg
+stringdata='I love ai python programming'
+EncBook={'1':'#','p':'@','o':'7','g':'$','I':'%','a':'8','t':'*','r':'3','n':'6'}
+encoding=EncryptFunc(stringdata)#전달된 문자열을 암호화 시켜서 반환하는 함수
+print(encoding)
+#위 암호화된 문자열 다시 복원시키는 함수 구현
+decmsg=DecryptFunc(encoding)
+print(decmsg)
