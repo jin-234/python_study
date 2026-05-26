4468f235317637c781854d6665f78cd2393a2e4e
jin-234 <kmjin041010@gmail.com>
2026-05-26 14:10:14 +0900

    클래스 디폴트 파라미터

--- 파이썬 공부 코딩 모음/클래스설계_예제4.py
+++ 파이썬 공부 코딩 모음/클래스설계_예제4.py
@@ -1,9 +1,14 @@
 class MyComInfo():
     def __init__(self,arg='Python Academy'): #디폴트 파라미터
-        self.arg=arg
+        self.name=arg
     def DisplayName(self):
-        print(self.arg)
+        print(self.name)
+    def SettingName(self,rename):
+        self.name=rename
 com1=MyComInfo("Ai Academy")
 com1.DisplayName() #Ai Academy
 com2=MyComInfo()
 com2.DisplayName()#Python Academy
+
+com2.SettingName("Agent Academy")
+com2.DisplayName()#Agent Academyac
